510c8d8ea4b1cf0c390b8c9313ee1f4650f76de7
Tom Burzynski <tellur.beryl@gmail.com>
2023-04-18 18:41:45 +0100

    add GET http request

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ app.use(express.json());
 // ------
 
 module.exports = app;
+
+// GET
+app.get('/', (req, res) => {
+	res.sendFile('./index.html');
+});
+
+app.get('/questions', (req, res) => {
+	res.send(questions);
+});
+
+app.get('questions/:id', (req, res) => {
+	const idx = Number(req.params.id);
+	const question = questions[idx - 1];
+
+	!question || typeof idx !== 'number'
+		? res.status(404).json({
+				Error: 'There is no question with the given ID',
+		  })
+		: res.send(question);
+});
