8fbfd2cc27895b714c440fdf8c346d7fd296891c
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-04-21 13:43:55 +0100

    revert changes

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const site = "https://duck-server.onrender.com"
 
-app.listen(site, () => {
+app.listen(port, () => {
 	console.log(`Server started at port ${port}`);
 });
